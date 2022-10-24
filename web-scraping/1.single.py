9922e61a1047253ea34b05c2515cabb5c06ed17e
Piotr, Przytula <piotr.przytula@intel.com>
2022-10-24 17:46:59 +0200

    Webscraping init

---
+++ web-scraping/1.single.py
@@ -0,0 +1,13 @@
+import requests
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+from bs4 import BeautifulSoup 
+
+url = "https://www.scrapethissite.com/pages/simple/"
+
+def main():
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.text, "lxml")
+    
+
+if __name__ == "__main__":
+    main()
---
+++ web-scraping/2.paginated.py
@@ -0,0 +1,16 @@
+from argparse import ArgumentParser
+import requests
+import sys
+from typing import List
+from bs4 import BeautifulSoup 
+
+url = "https://www.scrapethissite.com/pages/forms/"
+
+
+def main(term: str) -> List[List]:
+    data = []
+
+    return data
+
+if __name__ == "__main__":
+    main(sys.argv[0])
